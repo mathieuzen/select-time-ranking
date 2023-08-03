de288c57a9a7fa7fb3629d1baea9168968c90b6f
mathieuzen <zen.mathieu@gmail.com>
2023-08-04 01:10:49 +0200

    data edited

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 techniques = [
     {
-        name: "Sélectionner une ou plusieurs saisons",
-        description: "Dans l'expérience suivante, vous serez amenés à choisir quelle solution vous semble la plus pertinente pour sélectionner une ou plusieurs saisons (automne, hiver, printemps, été). La préférence est donnée en comparaison par paire, vous devrez choisir pour chaque paire la solution préférée."
+        name: "Sélectionner un moment précis dans le temps",
+        description: "Dans l'expérience suivante, vous serez amenés à choisir quelle solution vous semble la plus pertinente pour sélectionner un moment précis dans le temps. La préférence est donnée en comparaison par paire, vous devrez choisir pour chaque paire la solution préférée."
     },
 ];
 
